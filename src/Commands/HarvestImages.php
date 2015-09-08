8697169f81d3d2c202718bcca513459a242737ba
Jeremy Tubbs <jeremytubbs@gmail.com>
2015-09-08 19:30:15 +0000

    manage assets on update

--- src/Commands/HarvestImages.php
+++ src/Commands/HarvestImages.php
@@ -35,9 +35,8 @@ class HarvestImages extends Job implements SelfHandling, ShouldQueue
             'path' => public_path('images/'.$object_id),
             'driver' => 'imagick'
         ]);
-        $this->filesystem = new Filesystem(new Local(public_path('images')));
+        $this->filesystem = new Filesystem(new Local(public_path()));
         $this->imageManager = new ImageManager(['driver' => 'imagick']);
-        $this->update = false;
     }
 
     /**
@@ -51,23 +50,15 @@ class HarvestImages extends Job implements SelfHandling, ShouldQueue
         $object = Object::with('assets', 'assets.type')->findOrFail($this->object_id);
 
         // delete directory if it exists
-        if ($this->filesystem->has($object->id)) {
-            $this->filesystem->deleteDir($object->id);
-            // mark as update to handle possible asset delete
-            $this->update = true;
+        if ($this->filesystem->has('images/'.$object->id)) {
+            $this->filesystem->deleteDir('images/'.$object->id);
+            $this->deleteAssetRecords($object);
         }
 
         foreach ($object->assets as $asset) {
             if($asset->type->asset_type_name == 'source') {
                 // if object has proper rights
                 if ($object->can_zoom == 1 && $object->can_download == 1) {
-
-                    if ($this->update) {
-                        // get restrict id and delete assets if rights permission has changed
-                        $restrict_type_id = AssetType::where('asset_type_name', '=', 'restrict')->pluck('id');
-                        Asset::where('asset_type_id', $restrict_type_id)->where('source_id', '=', $asset->source_id)->delete();
-                    }
-
                     // get result back form deepzoom
                     $result = $this->deepzoom->makeTiles($asset->asset_file_uri, 'dzi'.$asset->asset_sequence, $asset->asset_sequence);
                     foreach ($result['data'] as $key => $value) {
@@ -87,34 +78,13 @@ class HarvestImages extends Job implements SelfHandling, ShouldQueue
                             $constraint->upsize();
                         });
                         $img->encode('jpg');
-                        $imgPath = 'images/'.$object->id.'/'.$asset->asset_sequence.'_'.$key.'.jpg';
+                        $imgPath = 'images/'.$object->id.'/'.$asset->asset_sequence.'/'.$asset->asset_sequence.'_'.$key.'.jpg';
                         $this->filesystem->put($imgPath, $img);
                         $this->createAsset($imgPath, $asset_type_id, $object->id, $asset->asset_sequence, $asset->source_id);
                     }
                 // generate the sizes below the restricted width
                 } else {
-                    // setup array of type ids to be deleted if this is an update
-                    if ($this->update) {
-                        $type_ids = [];
-                        $type_id = AssetType::where('asset_type_name', '=', 'dzi')->pluck('id');
-                        if (! in_array($type_id, $type_ids, true)) {
-                            array_push($type_ids, $type_id);
-                        }
-                        $type_id = AssetType::where('asset_type_name', '=', 'jsonp')->pluck('id');
-                        if (! in_array($type_id, $type_ids, true)) {
-                            array_push($type_ids, $type_id);
-                        }
-                    }
                     foreach ($this->sizes as $key => $value) {
-
-                        if ($this->update && $value >= config('harvester.restrict.width')) {
-                            // get existing ids and delete assets if rights permission change
-                            $type_id = AssetType::where('asset_type_name', '=', $key)->pluck('id');
-                            if (! in_array($type_id, $type_ids, true)) {
-                                array_push($type_ids, $type_id);
-                            }
-                        }
-
                         if ($value <= config('harvester.restrict.width')) {
                             $asset_type_id = AssetType::where('asset_type_name', '=', $key)->pluck('id');
                             $img = $this->imageManager->make($asset->asset_file_uri);
@@ -124,7 +94,7 @@ class HarvestImages extends Job implements SelfHandling, ShouldQueue
                                 $constraint->upsize();
                             });
                             $img->encode('jpg');
-                            $imgPath = 'images/'.$object->id.'/'.$asset->asset_sequence.'_'.$key.'.jpg';
+                            $imgPath = 'images/'.$object->id.'/'.$asset->asset_sequence.'/'.$asset->asset_sequence.'_'.$key.'.jpg';
                             $this->filesystem->put($imgPath, $img);
                             $this->createAsset($imgPath, $asset_type_id, $object->id, $asset->asset_sequence, $asset->source_id);
                         }
@@ -137,26 +107,47 @@ class HarvestImages extends Job implements SelfHandling, ShouldQueue
                         $constraint->upsize();
                     });
                     $img->encode('jpg');
-                    $imgPath = 'images/'.$object->id.'/'.$asset->asset_sequence.'_restrict.jpg';
+                    $imgPath = 'images/'.$object->id.'/'.$asset->asset_sequence.'/'.$asset->asset_sequence.'_restrict.jpg';
                     $this->filesystem->put($imgPath, $img);
                     $this->createAsset($imgPath, $asset_type_id, $object->id, $asset->asset_sequence, $asset->source_id);
-
-                    if ($this->update) {
-                        foreach ($type_ids as $id) {
-                            Asset::where('asset_type_id', $id)->where('source_id', '=', $asset->source_id)->delete();
-                        }
-                    }
                 }
             }
         }
     }
 
     public function createAsset($imgPath, $asset_type_id, $object_id, $sequence, $source_id) {
-            $asset = Asset::firstOrNew(['asset_file_uri' => $imgPath]);
-            $asset->asset_type_id = $asset_type_id;
-            $asset->object_id = $object_id;
-            $asset->asset_sequence = $sequence;
-            $asset->source_id = $source_id;
-            $asset->save();
+        $asset = Asset::firstOrNew(['asset_file_uri' => $imgPath]);
+        $asset->asset_type_id = $asset_type_id;
+        $asset->object_id = $object_id;
+        $asset->asset_sequence = $sequence;
+        $asset->source_id = $source_id;
+        $asset->save();
+    }
+
+    public function deleteAssetRecords($object) {
+        $asset_ids = [];
+        foreach ($object->assets as $asset) {
+            if($asset->type->asset_type_name != 'source') {
+                // if object has proper rights
+                if ($object->can_zoom == 1 && $object->can_download == 1) {
+                    if ($asset->type->asset_type_name == 'restrict') {
+                        array_push($asset_ids, $asset->id);
+                    }
+                } else {
+                    if ($asset->type->asset_type_name != 'restrict') {
+                        foreach ($this->sizes as $key => $value) {
+                            if (strtolower($key) == $asset->type->asset_type_name) {
+                                if ($value >= config('harvester.restrict.width')) {
+                                    array_push($asset_ids, $asset->id);
+                                }
+                            }
+                        }
+                        if ($asset->type->asset_type_name == 'dzi') array_push($asset_ids, $asset->id);
+                        if ($asset->type->asset_type_name == 'jsonp') array_push($asset_ids, $asset->id);
+                    }
+                }
+            }
+        }
+        Asset::destroy($asset_ids);
     }
 }
