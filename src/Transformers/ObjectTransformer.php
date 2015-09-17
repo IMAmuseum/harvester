17425097654cb0f75b5132332566135118f3d569
Jeremy Tubbs <jeremytubbs@gmail.com>
2015-09-17 02:49:54 +0000

    api transform actor dates and locations

--- src/Transformers/ObjectTransformer.php
+++ src/Transformers/ObjectTransformer.php
@@ -28,7 +28,9 @@ class ObjectTransformer
                     'death_date'        => $actor_value->death_date,
                     'birth_location'    => $actor_value->birth_location,
                     'work_location'     => $actor_value->work_location,
-                    'death_location'    => $actor_value->death_location
+                    'death_location'    => $actor_value->death_location,
+                    'dates'             => $this->transformDates($actor_value->dates),
+                    'locations'         => $this->transformDates($actor_value->locations),
 
                ];
             }
@@ -46,38 +48,9 @@ class ObjectTransformer
             $asset_group[$asset_item_value->source->origin_id] = $asset_transform;
         }
 
-        $dates = null;
-        if (! empty($object->dates)) {
-            $object_dates = $object->dates;
-            $object_dates = $object_dates->groupBy('date_type_id');
-            foreach ($object_dates as $date_type) {
-                $date_transform = null;
-                foreach ($date_type as $value) {
-                    $date_transform[]= [
-                        'date' => $value->date,
-                        'timestamp' => $value->date_at
-                    ];
-                }
-                $dates[$value->type->date_type_name] = $date_transform;
-            }
-        }
+        $dates = $this->transformDates($object->dates);
 
-        $locations = null;
-        if (! empty($object->locations)) {
-            $object_locations = $object->locations;
-            $object_locations = $object_locations->groupBy('location_type_id');
-            foreach ($object_locations as $location_type) {
-                $location_transform = null;
-                foreach ($location_type as $value) {
-                    $location_transform[]= [
-                        'name' => $value->location,
-                        'latitude' => $value->latitude,
-                        'longitude' => $value->longitude
-                    ];
-                }
-                $locations[$value->type->location_type_name] = $location_transform;
-            }
-        }
+        $locations = $this->transformLocations($object->locations);
 
         $terms = null;
         if (! empty($object->terms)) {
@@ -145,6 +118,45 @@ class ObjectTransformer
             'created_at'        => $object->created_at,
             'updated_at'        => $object->updated_at,
         ];
+    }
+
+    public function transformDates($object_dates)
+    {
+        $dates = null;
+        if (! empty($object_dates)) {
+            $object_dates = $object_dates->groupBy('date_type_id');
+            foreach ($object_dates as $date_type) {
+                $date_transform = null;
+                foreach ($date_type as $value) {
+                    $date_transform[]= [
+                        'date' => $value->date,
+                        'timestamp' => $value->date_at
+                    ];
+                }
+                $dates[$value->type->date_type_name] = $date_transform;
+            }
+        }
+        return $dates;
+    }
+
+    public function transformLocations($object_locations)
+    {
+        $locations = null;
+        if (! empty($object_locations)) {
+            $object_locations = $object_locations->groupBy('location_type_id');
+            foreach ($object_locations as $location_type) {
+                $location_transform = null;
+                foreach ($location_type as $value) {
+                    $location_transform[]= [
+                        'name' => $value->location,
+                        'latitude' => $value->latitude,
+                        'longitude' => $value->longitude
+                    ];
+                }
+                $locations[$value->type->location_type_name] = $location_transform;
+            }
+        }
+        return $locations;
     }
 
     public function collection($objects)
